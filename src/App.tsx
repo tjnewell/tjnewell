71dc9c2b5aae1a01606dff4a32646991bfc3e9ce
TJ Newell <tj.newell@gmail.com>
2024-05-03 15:40:40 -0700

    Created Main Page Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { Container, Link, Box } from '@radix-ui/themes';
+import { Container, Link, Box, Text, Flex, Button } from '@radix-ui/themes';
 import { Routes, Route, Outlet, Link as RouterLink } from 'react-router-dom';
+import tj from './assets/tj-avatar.png';
+import { useState } from 'react';
 
 export default function App() {
   return (
@@ -14,10 +16,43 @@ export default function App() {
 }
 
 function Page() {
+  const [time, setTime] = useState('light');
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-16">
-      <Container size="4">
-        <Outlet />
+      <Container size="4" align="center">
+        <Flex gap="4" direction="column" align="center">
+          <Box height="400px" width="400px">
+            <img src={tj} />
+          </Box>
+          <Box width="400px">
+            <Flex direction="column" gap="4">
+              <Text align="center" as="div" size="6" weight="medium">
+                Hey! I'm TJ.
+              </Text>
+              <Text wrap="wrap" align="center" as="div">
+                Let's go on a fishing trip and get to know each other a little
+                bit more. But before we just head out, let's nail down some
+                details...
+              </Text>
+              <Text wrap="wrap" align="center" as="div" weight="medium">
+                Do you prefer fishing in the morning or the evening?
+              </Text>
+            </Flex>
+          </Box>
+          <Box width="400px">
+            <Flex gap="6" direction="row" justify="center">
+              <Button size="4" onClick={() => setTime('light')}>
+                Morning
+              </Button>
+              <Button size="4" onClick={() => setTime('dark')}>
+                Evening
+              </Button>
+            </Flex>
+          </Box>
+        </Flex>
+
+        {/* <Outlet /> */}
       </Container>
     </div>
   );
